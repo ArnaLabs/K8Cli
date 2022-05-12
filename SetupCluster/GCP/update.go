72566e3a71bcf62d250b4a0587cfba92d9ac4756
Jayadeep KM <kmjayadeep@gmail.com>
2022-05-12 14:31:06 +0200

    update labels
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- SetupCluster/GCP/update.go
+++ SetupCluster/GCP/update.go
@@ -3,14 +3,24 @@ package GCP
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	containerpb "google.golang.org/genproto/googleapis/container/v1"
 )
 
 func (g *GcpClient) UpdateCluster(ctx context.Context, cl *containerpb.Cluster) error {
+	name := "projects/" + g.Cluster.Cloud.Project + "/locations/" + g.Cluster.Cloud.Region + "/clusters/" + cl.Name
+	if !reflect.DeepEqual(cl.ResourceLabels, g.Cluster.Cluster.Labels) {
+		err := g.UpdateLabels(ctx, cl, name)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
 	if !cl.AddonsConfig.HttpLoadBalancing.Disabled != g.Cluster.Cluster.HttpLoadBalancer {
 		fmt.Println("k8s HTTP LB config changed, updating")
-		err := g.UpdateAddons(ctx, cl)
+		err := g.UpdateAddons(ctx, cl, name)
 		if err != nil {
 			return err
 		}
@@ -19,7 +29,7 @@ func (g *GcpClient) UpdateCluster(ctx context.Context, cl *containerpb.Cluster) 
 
 	if cl.GetCurrentMasterVersion() != g.Cluster.Master.KubernetesVersion {
 		fmt.Println("k8s master version changed, updating")
-		err := g.UpdateMaster(ctx, cl)
+		err := g.UpdateMaster(ctx, cl, name)
 		if err != nil {
 			return err
 		}
@@ -30,8 +40,7 @@ func (g *GcpClient) UpdateCluster(ctx context.Context, cl *containerpb.Cluster) 
 	return nil
 }
 
-func (g *GcpClient) UpdateAddons(ctx context.Context, cl *containerpb.Cluster) error {
-	name := "projects/" + g.Cluster.Cloud.Project + "/locations/" + g.Cluster.Cloud.Region + "/clusters/" + cl.Name
+func (g *GcpClient) UpdateAddons(ctx context.Context, cl *containerpb.Cluster, name string) error {
 	req := &containerpb.UpdateClusterRequest{
 		Name: name,
 		Update: &containerpb.ClusterUpdate{
@@ -52,8 +61,7 @@ func (g *GcpClient) UpdateAddons(ctx context.Context, cl *containerpb.Cluster) e
 	return g.WaitForClusterOperation(ctx, op)
 }
 
-func (g *GcpClient) UpdateMaster(ctx context.Context, cl *containerpb.Cluster) error {
-	name := "projects/" + g.Cluster.Cloud.Project + "/locations/" + g.Cluster.Cloud.Region + "/clusters/" + cl.Name
+func (g *GcpClient) UpdateMaster(ctx context.Context, cl *containerpb.Cluster, name string) error {
 	req := &containerpb.UpdateClusterRequest{
 		Name: name,
 		Update: &containerpb.ClusterUpdate{
@@ -67,5 +75,21 @@ func (g *GcpClient) UpdateMaster(ctx context.Context, cl *containerpb.Cluster) e
 	}
 
 	fmt.Printf("Master version updation initiated, status : %d\nop:%v\n", op.GetStatus(), op)
+	return g.WaitForClusterOperation(ctx, op)
+}
+
+func (g *GcpClient) UpdateLabels(ctx context.Context, cl *containerpb.Cluster, name string) error {
+	req := &containerpb.SetLabelsRequest{
+		Name:             name,
+		ResourceLabels:   g.Cluster.Cluster.Labels,
+		LabelFingerprint: cl.LabelFingerprint,
+	}
+
+	op, err := g.ClusterManagerClient.SetLabels(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Addons updation initiated, status : %d\nop:%v\n", op.GetStatus(), op)
 	return g.WaitForClusterOperation(ctx, op)
 }
